02efcc4d3c6f5afa9ef4821794af93ba9a2cb6fc
JR Conlin <src+github@jrconlin.com>
2023-05-11 15:43:27 -0700

    update other exp to string

--- vapid.js
+++ vapid.js
@@ -152,7 +152,7 @@ class VapidToken {
             throw new Error(this.lang.errs.ERR_NO_KEYS);
         }
         if (!claims.hasOwnProperty("exp")) {
-            claims.exp = parseInt(Date.now() * .001) + 300;
+            claims.exp = (parseInt(Date.now() * .001) + 300).toString();
         }
         if (!claims.hasOwnProperty("sub")) {
             throw new Error(this.lang.errs.ERR_CLAIM_MIS, "sub");
